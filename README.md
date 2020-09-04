183f5339418b46c5e2eb8c8635ab025ba6c0fda5
Nathan Johnson <nate.johnson2010@me.com>
2020-09-04 17:36:10 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # web_front_end
-Game Night Wizard Web Frontend
+Playlister Web Frontend
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import {BrowserRouter, Route, hashHistory} from 'react-router-dom';
-import Collection from './components/Collection';
 import Home from './components/Home';
-import LogIn from './components/LogIn';
 
 // import more components
 export default (
     <BrowserRouter>
         <div>
             <Route exact path='/' component={() => <Home />}/>
-            <Route path='/login' component={LogIn}/>
-            {/*<Route path='/join_game_night' component={() => <Home {...(content.dataset)} />}/>*/}
-            {/*<Route path='/my_collection' component={() => <Collection {...(content.dataset)} />}/>*/}
         </div>
     </BrowserRouter>
 );
